87c69903272bda5634d4232ec7fc3298c073a8c2
ayush24-08 <ayushbaghel2408@gmail.com>
2025-07-02 19:18:24 +0530

    GUESS NUMBERS

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
-import random 
-computer = random.choice([-1,0,1])
-youstr = input("ENTER THE CHOICE :")
-youDict =  {"s":1,"w":-1,"g":0}
-revdict = {1 : "SNAKE",-1: "WATER",0 :"GUN"}
+import random
+n = random.randint(1,100)
+a= -1
+guess = 1
 
-you =youDict[youstr]
+while(a!= n):
 
-# by nkow we have two variables that was you and computer 
+    a = int(input("ENTER THE NUMBER :"))
 
-print(f"YOU CHOOSE {revdict[you]}\n COMPUTER CHOOSE {revdict[computer]}")
+    if(a>n):
+        print("ENTER THE LOWER NUMBER :")
+        guess += 1 
 
-if (computer == you ):
-    print("ITS A DRAW!")
+    elif(a<n):
+        print("ENTER THE HIGHER NUMBER :")
+        guess += 1 
 
-else:
-    
-    if(computer == -1 and you == 1):
-         print("YOU WON!")
-         
-    elif(computer == -1 and you == 0):
-         print("YOU LOOSE!")
-
-    elif(computer == 1 and you == 0):
-         print("YOU WON!")
-
-    elif(computer == 1 and you == -1):
-         print("YOU LOOSE!")
-         
-    elif(computer == 0 and you == -1):
-         print("YOU WIN!")
-
-    elif(computer == 0 and you == 1):
-         print("YOU LOOSE!")
-
-
-    else:
-         print("somegthing went wrong")
-         
-         
-# ### short cut
-
-
-# if(computer-you == -1 or 2):
-    #  print("YOU LOSE")   
-
-    # else:
-    #  print("YOU WON")
+print(f" THE NUMBER IS {n} AND THE NUMBER OF YOUR ATTEMPS IS {guess}")
